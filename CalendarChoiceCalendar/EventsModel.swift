e913c21d82692cd34d54aa0464aa9af35d14ce80
shi-n <shi-n_xyz@cloudsquare.jp>
2020-10-22 07:03:03 +0900

    update Request Access

--- CalendarChoiceCalendar/EventsModel.swift
+++ CalendarChoiceCalendar/EventsModel.swift
@@ -13,43 +13,45 @@ class EventsModel: ObservableObject {
     @Published var events: [EKEvent] = []
     @Published var nextEvents: [EventDispModel] = []
     var offCalendar: [String] = []
+    let eventStore = EKEventStore()
     
     init() {
+        eventStore.requestAccess(to: .event) { (access, _) in
+            print("EKEventStore requestAccess: \(access)")
+        }
         self.updateNextEvents()
     }
     
     func updateNextEvents() {
         if let offs = UserDefaults.standard.stringArray(forKey: "offCalendar") {
             offCalendar = offs
+            print(offCalendar)
         }
         
         self.nextEvents = []
-        let eventStore = EKEventStore()
-//        eventStore.requestAccess(to: .event) { _,_ in
-            var calendars = eventStore.calendars(for: .event)
-            calendars.sort() { (a,b) in
-                a.title < b.title
+        var calendars = eventStore.calendars(for: .event)
+        calendars.sort() { (a,b) in
+            a.title < b.title
+        }
+        var index = 0
+        for calendar in calendars {
+            var isOn = true
+            if self.offCalendar.contains(calendar.title) {
+                isOn = false
             }
-            var index = 0
-            for calendar in calendars {
-                var isOn = true
-                if offCalendar.contains(calendar.title) {
-                    isOn = false
-                }
-                let predicate = eventStore.predicateForEvents(withStart: Date(), end: Date()  + (86400 * 365), calendars: [calendar])
-                let events = eventStore.events(matching: predicate)
-                if 0 < events.count {
-                    let event = EventDispModel(index: index, startDate: events[0].startDate, eventTitle: events[0].title, calendar: calendar, isOn: isOn)
-                    self.nextEvents.append(event)
-                }
-                else {
-                    let event = EventDispModel(index: index, startDate: Date(), eventTitle: "", calendar: calendar, isOn: isOn)
-                    self.nextEvents.append(event)
-                }
-                index = index + 1
-                print(calendar)
+            let predicate = eventStore.predicateForEvents(withStart: Date(), end: Date()  + (86400 * 365), calendars: [calendar])
+            let events = eventStore.events(matching: predicate)
+            if 0 < events.count {
+                let event = EventDispModel(index: index, startDate: events[0].startDate, eventTitle: events[0].title, calendar: calendar, isOn: isOn)
+                self.nextEvents.append(event)
             }
-//        }
+            else {
+                let event = EventDispModel(index: index, startDate: Date(), eventTitle: "", calendar: calendar, isOn: isOn)
+                self.nextEvents.append(event)
+            }
+            index = index + 1
+            print(calendar)
+        }
     }
     
     func updateOffCalendar() {
@@ -60,20 +62,6 @@ class EventsModel: ObservableObject {
             }
         }
         UserDefaults.standard.setValue(offCalendar, forKey: "offCalendar")
-    }
-    
-    func updateEvents() {
-        self.events = []
-        let eventStore = EKEventStore()
-        let predicate = eventStore.predicateForEvents(withStart: Date(), end: Date()  + (86400 * 365), calendars: nil)
-        eventStore.requestAccess(to: .event) { _,_ in
-            let eventArray = eventStore.events(matching: predicate)
-            for event in eventArray {
-                if event.calendar.title == "FC Barcelona" {
-                    self.events.append(event)
-                }
-            }
-        }
     }
     
     static func dateDisp(date: Date) -> String {
