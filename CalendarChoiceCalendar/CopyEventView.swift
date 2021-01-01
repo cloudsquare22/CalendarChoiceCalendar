5da3acea2a9b0d69d4249f517d8d72b25542c3c5
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-02 08:22:09 +0900

    Update Copy event.

--- CalendarChoiceCalendar/CopyEventView.swift
+++ CalendarChoiceCalendar/CopyEventView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct CopyEventView: View {
     let event: EventDispModel
-    
+    @EnvironmentObject var eventsModel: EventsModel
+    @State var selection = 0
+
     var body: some View {
         VStack {
-            Text("Copy events to other calendars.")
-            
-
+            Text(self.event.calendar.title)
+                .foregroundColor(Color(self.event.calendar.cgColor))
+            Text(self.event.eventTitle)
+            Text(EventsModel.dateDisp(date: self.event.startDate, isAllDay: self.event.isAllDay))
+            Picker(selection: self.$selection, label: Text("Calendar"), content: {
+                ForEach(0..<self.eventsModel.nextEvents.count) { index in
+                    Text(self.eventsModel.nextEvents[index].calendar.title)
+                        .tag(index)
+                }
+            })
+//            .labelsHidden()
         }
+        .padding()
     }
 }
 
