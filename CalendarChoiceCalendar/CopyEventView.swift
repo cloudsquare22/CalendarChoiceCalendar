09da5266d93e23a2375664e4dd6986ced9fc08d3
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-03 08:04:33 +0900

    Add Copy Calendar.

--- CalendarChoiceCalendar/CopyEventView.swift
+++ CalendarChoiceCalendar/CopyEventView.swift
@@ -16,17 +16,26 @@ struct CopyEventView: View {
         VStack {
             Text(self.event.calendar.title)
                 .foregroundColor(Color(self.event.calendar.cgColor))
+                .font(.title3)
             Text(self.event.eventTitle)
+                .font(.title3)
             Text(EventsModel.dateDisp(date: self.event.startDate, isAllDay: self.event.isAllDay))
+                .font(.title3)
             Picker(selection: self.$selection, label: Text("Calendar"), content: {
                 ForEach(0..<self.eventsModel.nextEvents.count) { index in
                     Text(self.eventsModel.nextEvents[index].calendar.title)
                         .tag(index)
+                        .padding()
                 }
             })
-//            .labelsHidden()
+            Button(action: {
+                EventsModel.copyCalendar(eventDispModel: self.event, selectCalendar: self.eventsModel.nextEvents[self.selection].calendar)
+            }, label: {
+                Text("Copy to calendar.")
+            })
+            .font(.title3)
         }
-        .padding()
+        .padding(8)
     }
 }
 
